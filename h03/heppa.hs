3cbd71ed5e9f69eb1f8dd3bc1d52efceeb6f4e9b
Tuomas Starck <dev@starck.fi>
2011-11-11 13:00:54 +0200

    Heppa hopottaa liikaa.

--- h03/heppa.hs
+++ h03/heppa.hs
@@ -4,7 +4,11 @@ import Debug.Trace
 
 abc = "ABCDEFGHIJKLMNOPQRSTUVWXY"
 
-loc = [-11, -9, -7, -3, 3, 7, 9, 11]
+precalc = [ [7, 11], [8, 10, 12], [5, 9, 11, 13], [6, 12, 14], [7, 13],
+            [2, 12, 16], [3, 13, 15, 17], [0, 4, 10, 14, 16, 18], [1, 11, 17, 19], [2, 12, 18],
+            [1, 7, 17, 21], [0, 2, 8, 18, 20, 22], [1, 3, 5, 9, 15, 19, 21, 23], [2, 4, 6, 16, 22, 24], [3, 7, 17, 23],
+            [6, 12, 22], [5, 7, 13, 23], [6, 8, 10, 14, 20, 24], [7, 9, 11, 21], [8, 12, 22],
+            [11, 17], [10, 12, 18], [11, 13, 15, 19], [12, 14, 16], [13, 17] ]
 
 inc = (+ 1)
 
@@ -12,15 +16,17 @@ insert :: Int -> Int -> [Char] -> [Char]
 insert i x board = take x board ++ [abc !! i] ++ drop (inc x) board
 
 heppa' :: Int -> Int -> [Char] -> Int
-heppa' i x board = if i <3                                                             -- ei valmis, jaksa solvaamista
-                     then if 0 <= x && x < 25 && board !! x == ' '                     -- löydettiin tyhjä paikka laudalta
+heppa' i x board = if i < 25
+                     then if board !! x == ' '
                             then sum
                                    (zipWith3 heppa'
                                      (repeat (inc i))
-                                     (map (+ x) loc)
+                                     (precalc !! x)
                                      (repeat (insert i x board)))
                             else 0
-                     else trace board 1
+                     else 1
 
 
 heppa = heppa' 0 0 (take 25 (repeat ' '))
+
+main = putStrLn (show heppa)
