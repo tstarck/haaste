87f3d11df875f37594ce820a345bdc0f44a61d2e
Tuomas Starck <dev@starck.fi>
2011-11-18 15:55:27 +0200

    Apuskriptin tarkistus ja säätöä

--- h04/sumpot.sh
+++ h04/sumpot.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ ! -f sumpot.bc ]; then
+	echo error: sumpot.bc is missing
+	exit 1
+fi
+
 sumpot() {
 	for NRO in $(seq $1 $2); do
-		for TULO in $(echo "p($NRO)" | bc sumpot.bc); do
+		for TULO in $(echo "e=p($NRO)" | bc sumpot.bc); do
 			SUM=$(echo $TULO | sed 's/\(.\)/+\1/g;s/+//' | bc)
 			if [ "$NRO" == "$SUM" ]; then
 				echo $TULO
@@ -11,13 +16,13 @@ sumpot() {
 	done
 }
 
-sumpot   2  20 &
-sumpot  21  52 &
-sumpot  53  94 &
-sumpot  95 156 &
-sumpot 157 228 &
-sumpot 229 300 &
-sumpot 301 382 &
-sumpot 383 450 &
+sumpot   2  24 &
+sumpot  25  60 &
+sumpot  61 102 &
+sumpot 103 162 &
+sumpot 163 230 &
+sumpot 231 301 &
+sumpot 302 378 &
+sumpot 379 450 &
 
 wait && exit 0
