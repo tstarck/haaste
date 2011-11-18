81445d2c5c3e07bca4a3090d8f479b4f1b02d143
Tuomas Starck <dev@starck.fi>
2011-11-18 15:39:06 +0200

    Ratkaisu saattaisi toimia

--- h04/sumpot.sh
+++ h04/sumpot.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-for FOO in $(seq 8 18); do
-	echo FOO on $FOO
-	for nro in $(echo "pows($FOO)" | bc sumpot.bc); do
-		SUM=$(echo $nro | sed 's/\(.\)/+\1/g;s/+//' | bc)
-		if [ "$FOO" == "$SUM" ]; then
-			echo jee $FOO ja $nro
-		fi
+sumpot() {
+	for NRO in $(seq $1 $2); do
+		for TULO in $(echo "p($NRO)" | bc sumpot.bc); do
+			SUM=$(echo $TULO | sed 's/\(.\)/+\1/g;s/+//' | bc)
+			if [ "$NRO" == "$SUM" ]; then
+				echo $TULO
+			fi
+		done
 	done
-done
+}
+
+sumpot   2  20 &
+sumpot  21  52 &
+sumpot  53  94 &
+sumpot  95 156 &
+sumpot 157 228 &
+sumpot 229 300 &
+sumpot 301 382 &
+sumpot 383 450 &
+
+wait && exit 0
