fea64dab27e6a717a89ab04baf139a03349967dc
Tuomas Starck <dev@starck.fi>
2011-11-21 02:26:37 +0200

    Rinnakkaisuudesta luovuttu ja bc tehokäytössä.

--- h04/sumpot.sh
+++ h04/sumpot.sh
@@ -5,24 +5,8 @@ if [ ! -f sumpot.bc ]; then
 	exit 1
 fi
 
-sumpot() {
-	for NRO in $(seq $1 $2); do
-		for TULO in $(echo "e=p($NRO)" | bc sumpot.bc); do
-			SUM=$(echo $TULO | sed 's/\(.\)/+\1/g;s/+//' | bc)
-			if [ "$NRO" == "$SUM" ]; then
-				echo $TULO
-			fi
-		done
-	done
-}
+for NRO in $(seq 2 450); do
+	echo "e=p($NRO)" | bc sumpot.bc
+done | sort -n
 
-sumpot   2  24 &
-sumpot  25  60 &
-sumpot  61 102 &
-sumpot 103 162 &
-sumpot 163 230 &
-sumpot 231 301 &
-sumpot 302 378 &
-sumpot 379 450 &
-
-wait && exit 0
+exit 0
